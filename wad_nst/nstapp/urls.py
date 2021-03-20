b530c66ffa1f4698ed146f0bd903ef859a33a30d
shreyasms <shreyasms@phaidra.ai>
2021-03-20 14:53:17 +0530

    Make changes from ashar's repo

--- wad_nst/nstapp/urls.py
+++ wad_nst/nstapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="homepage"),
+    path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("login/", views.loginuser, name="logininuser"),
+    path("login/", views.loginuser, name="loginuser"),
     path("logout/", views.logoutuser, name="logoutuser"),
     path("signup/", views.signupuser, name="signupuser"),
 ]
