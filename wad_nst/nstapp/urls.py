029c3fd8c1f6cb5f6306fc338a61fe00c77bd32f
codernishchay <nishchay.vns2000@gmail.com>
2021-04-05 19:43:54 +0530

    added file upload

--- wad_nst/nstapp/urls.py
+++ wad_nst/nstapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,4 +11,8 @@ urlpatterns = [
     path("logout/", views.logoutuser, name="logoutuser"),
     path("signup/", views.signupuser, name="signupuser"),
     path("gallery/", views.gallery, name="gallery"),
+    path("upload/", views.upload, name="upload"),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static.(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
