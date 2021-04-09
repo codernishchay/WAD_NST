7837a37e628ea4c873cf8fff8b061d1d94952761
shreyasms <shreyasms@phaidra.ai>
2021-04-09 21:17:59 +0530

    Add Neural Style Transfer

---
+++ wad_nst/nstapp/ML/img_loader.py
@@ -0,0 +1,32 @@
+from . import *
+
+
+class ImageLoader:
+    def __init__(self, contentpath, stylepath, finalpath):
+
+        self.stylepath = stylepath
+        self.contentpath = contentpath
+        self.finalpath = finalpath
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
+        self.loader = transforms.Compose(
+            [transforms.Resize(imsize), transforms.ToTensor()]  # scale imported image
+        )
+
+    # transform it into a torch tensor
+
+    def image_loader(self, image_name):
+        image = Image.open(image_name)
+        image = self.loader(image).unsqueeze(0)
+
+        return image.to(device, torch.float)
+
+    # style_img = image_loader("./data/images/neural-style/picasso.jpg")
+    # content_img = image_loader("./data/images/neural-style/dancing.jpg")
+    def open_images(self):
+        style_img = image_loader(self.stylepath)
+        content_img = image_loader(self.contentpath)
+        assert style_img.size() == content_img.size()
+
+        return content_img, style_img
---
+++ wad_nst/nstapp/ML/img_unloader.py
@@ -0,0 +1,13 @@
+from . import *
+
+
+class Unloader:
+    def __init__(self):
+        self.unloader = transforms.ToPILImage()
+
+    def pil_img(self, tensor):
+        image = tensor.cpu().clone()
+        image = image.squeeze(0)  # remove the fake batch dimension
+        image = unloader(image)
+
+        return image
